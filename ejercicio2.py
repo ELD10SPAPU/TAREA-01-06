99991a969bff070d02e80e3bdc2a5f25d0d9664f
ELD10SPAPU <danielopazo18.04@gmail.com>
2024-06-01 14:08:16 -0400

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,17 +1,28 @@
-
-
 def contar_vocales_consonantes(palabra):
     contador_vocales = 0
     contador_consonantes = 0
-    vocales = "aeiou"
+    vocales = "aeiouAEIOU"
 
-    palabra=input("Ingrese una palabra")
-    
-    for letra in palabra.lower():
+    for letra in palabra:
         if letra.isalpha():
             if letra in vocales:
-                contador_vocales += 1
+                contador_vocales = contador_vocales + 1
             else:
-                contador_consonantes += 1
+                contador_consonantes = contador_consonantes + 1
 
     return contador_vocales, contador_consonantes
+
+def main():
+    palabras = []
+    while True:
+        palabra = input("Ingresa una palabra (o presione Enter para finalizar): ")
+        if not palabra:
+            break
+        palabras.append(palabra)
+
+    for palabra in palabras:
+        vocales, consonantes = contar_vocales_consonantes(palabra)
+        print("La palabra ", palabra, " tiene ", vocales, " vocales y ", consonantes, " consonantes")
+
+if __name__ == "__main__":
+    main()
